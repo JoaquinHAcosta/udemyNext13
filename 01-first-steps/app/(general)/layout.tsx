95e82fe6b12eac90238de872c15af456ae937628
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-12-16 21:16:22 -0300

    navbar

--- 01-first-steps/app/(general)/layout.tsx
+++ 01-first-steps/app/(general)/layout.tsx
@@ -1,11 +1,15 @@
+import { Navbar } from '@/components'
 import React from 'react'
 
 const GeneralLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex flex-col items-center p-24">
-      <span className="text-xl">Hola mundo</span>
-      {children}
-    </main>
+    <>
+      <Navbar />
+      <main className="flex flex-col items-center p-24">
+        <span className="text-xl">Hola mundo</span>
+        {children}
+      </main>
+    </>
   )
 }
 
